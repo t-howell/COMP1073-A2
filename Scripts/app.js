eb4551d9cfdb99b27e45756f10871993a6b5a36f
thowell <tamara.howell@hotmail.com>
2017-02-08 22:37:22 -0500

    Consolidating JS

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,7 +3,18 @@
 // Site: http://comp1073-a1.azurewebsites.net/
 // Description: Contains JavaScript for About Me page
 
+/* javascript lives here */
+"use strict";
 
+// IIFE - Immediately Invoked Function Expression
+
+(function () { // Beginning of the IIFE
+
+  console.log("App Started...");
+  console.info(`Page Title: ${document.title}`); // string interpolation
+
+  switch (document.title) {
+    case "Home":
 //reference an element
 var aboutMe = document.getElementById("aboutMe");
 
@@ -23,3 +34,111 @@ var aboutHeading = document.getElementById("aboutHeading");
 var headerContent = "About Me";
 
 aboutHeading.innerHTML = headerContent;
+
+
+      break;
+
+    case "Projects":
+
+//Page heading
+
+var projectsHeading = document.getElementById("projectsHeading");
+
+var headingContent = "My Projects";
+
+projectsHeading.textContent = headingContent;
+
+//First project heading
+
+var firstHeading = document.getElementById("firstProjectHeading");
+
+var firstHeadingContent = "Homeohealth Center";
+
+firstHeading.textContent = firstHeadingContent;
+
+//First project info
+
+var firstInfo = document.getElementById("firstProjectInfo");
+
+var firstInfoContent = "As the Homeohealth Center brand was emerging, I was able to create a logo and "
+					+ "original content for the owner.";
+
+firstInfo.textContent = firstInfoContent;
+
+//Second project heading
+
+var secondHeading = document.getElementById("secondProjectHeading");
+
+var secondHeadingContent = "Iris Group";
+
+secondHeading.textContent = secondHeadingContent;
+
+//Second project info
+
+var secondInfo = document.getElementById("secondProjectInfo");
+
+var secondInfoContent = "After the Iris Group lost contact with their designer, they requested my help in "
+					+ "creating new content. This involved designing brochures, mail labels and newsletters.";
+
+secondInfo.textContent = secondInfoContent;
+
+//Third project heading
+
+var thirdHeading = document.getElementById("thirdProjectHeading");
+
+var thirdHeadingContent = "Freehand Doodles";
+
+thirdHeading.textContent = thirdHeadingContent;
+
+//Third project info
+
+var thirdInfo = document.getElementById("thirdProjectInfo");
+
+var thirdInfoContent = "My freehand illustrations may not require any technical skill, but they are "
+				+ "useful in creating more casual content.";
+			
+thirdInfo.textContent = thirdInfoContent;
+
+	break;
+
+	case "Contact":
+
+// Form Main Heading
+
+var contactHeading = document.getElementById("contactHeading");
+
+var headingContent = "Let's Connect!";
+
+contactHeading.textContent = headingContent;
+
+//------------------  Form h3's  ------------------------------
+
+//Contact Name
+
+var contactName = document.getElementById("name");
+
+var nameHeading = "Name";
+
+contactName.textContent = nameHeading;
+
+//Contact Email
+
+var contactEmail = document.getElementById("email");
+
+var emailContent = "Email Address";
+
+contactEmail.textContent = emailContent;
+
+//Contact Message
+
+var contactMessage = document.getElementById("message");
+
+var messageContent = "Your message";
+
+contactMessage.textContent = messageContent;
+
+break;
+
+  }
+
+})(); // end of the IIFE
